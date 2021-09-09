75fa2dc70a780cb058c41cf744bd656f260129ba
Nadia Mohammed <nadia.halseh@gmail.com>
2021-09-09 20:25:29 +0300

    Modify (renderTempate) use (Regular Expression)+ Use (Template) html tag+Open (Patient Edit) page by click on (edit) button inside patient list (This is the last thing I did and  It has not been reviewed)

---
+++ app/main.js
@@ -0,0 +1,46 @@
+appInit = () => {
+    renderTable();
+    $(".router-link").click(onRouterLinkClick);
+    $(".edit-btn").click(onEditClick);
+}
+
+renderTable = () => {
+    var txt = $(".template-elm").html();
+    for (let i = 0; i < patientsData.length; i++) {
+        var tr = renderTemplate(txt, patientsData[i]);
+        $("tbody").append(tr);
+    }
+}
+
+renderTemplate = (str, patientDataObj) => {
+    var placeHolder = str.match(/\{{.+?}\}/g);
+    var token = str.match(/[^{\}]+(?=})/g);
+    for (let i = 0; i < token.length; i++) {
+        var patientData = patientDataObj[token[i]];
+        str = str.replace(placeHolder[i], patientData);
+    }
+    return str;
+}
+
+onRouterLinkClick = (e) => {
+    var desiredContent = $(e.target).data().target;
+    navigate(desiredContent);
+}
+
+navigate = (desiredContent) => {
+    hideAll();
+    $(desiredContent).show();
+}
+
+hideAll = () => {
+    $(".screen").hide();
+}
+
+onEditClick = (e) => {
+    var siblings = $(e.target).parent().siblings();
+    var userID = $(siblings).eq(0).text();
+    $(".user-id").text(userID);
+}
+
+$(document).ready(appInit);
+
